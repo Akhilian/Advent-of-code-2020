71ea8441d88ba1ff523c9a4fa9f47265993efa5f
Adrien SAUNIER <contact.adriensaunier@gmail.com>
2020-12-08 10:15:16 +0100

    :sparkles: Day 4 - ex 2

--- day_4_passport_scanner/passport_scanner.py
+++ day_4_passport_scanner/passport_scanner.py
@@ -1,9 +1,84 @@
+import re
+from functools import reduce
+
+from typing import List
+
 from common.file_reader import FileReader
 
 
+def _is_outdated(field: str, min: int, max: int):
+    return int(field) < min or int(field) > max
+
+
+def _parse_to_dict(acc: dict, data: List[str]) -> str:
+    acc[data[0]] = data[1]
+    return acc
+
+
 def is_passport_valid(passport: str) -> bool:
-    required_fields = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid']
-    return [field in passport for field in required_fields].count(False) == 0
+    field_definition = {
+        'byr': None,
+        'iyr': None,
+        'eyr': None,
+        'hgt': None,
+        'hcl': None,
+        'ecl': None,
+        'pid': None,
+    }
+
+    required_fields = set(field_definition)
+    is_all_fields_present = [field in passport for field in required_fields].count(False) == 0
+
+    if not is_all_fields_present:
+        return False
+
+    split = [field.split(':') for field in passport.split()]
+    fields = reduce(_parse_to_dict, split, {})
+
+    if fields['byr']:
+        if _is_outdated(fields['byr'], 1920, 2002):
+            return False
+
+    if fields['iyr']:
+        if _is_outdated(fields['iyr'], 2010, 2020):
+            return False
+
+    if fields['eyr']:
+        if _is_outdated(fields['eyr'], 2020, 2030):
+            return False
+
+    if fields['hgt']:
+        height_and_unit = re.search('(\d+)(in|cm)', fields['hgt'])
+
+        if not height_and_unit:
+            return False
+
+        height = int(height_and_unit.group(1))
+        unit = height_and_unit.group(2)
+
+        if unit == 'in':
+            if height > 76 or height <= 59:
+                return False
+
+        if unit == 'cm':
+            if height < 150 or height > 193:
+                return False
+
+    if fields['hcl']:
+        if not re.match('#[\da-f]{6}', fields['hcl']):
+            return False
+
+    if fields['ecl']:
+        authorized_colors = ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth']
+        if fields['ecl'] not in authorized_colors:
+            return False
+
+    if fields['pid']:
+        if not re.match('^\d{9}$', fields['pid']):
+            return False
+
+    return True
+
 
 if __name__ == '__main__':
     file_reader = FileReader('./input')
--- day_4_passport_scanner/passport_scanner_test.py
+++ day_4_passport_scanner/passport_scanner_test.py
@@ -1,3 +1,5 @@
+import pytest
+
 from day_4_passport_scanner.passport_scanner import is_passport_valid
 
 
@@ -12,27 +14,6 @@ byr:1937 iyr:2017 cid:147 hgt:183cm
         # then
         assert is_passport_valid(passport)
 
-
-    def test_when_passport_is_invalid_when_byr_is_missing(self):
-        # given
-        passport = """
-ecl:gry pid:860033327 eyr:2020 hcl:#fffffd
-iyr:2017 cid:147 hgt:183cm
-        """
-
-        # then
-        assert is_passport_valid(passport) is False
-
-    def test_when_passport_is_invalid_when_iyr_is_missing(self):
-        # given
-        passport = """
-ecl:gry pid:860033327 eyr:2020 hcl:#fffffd
-byr:1937 cid:147 hgt:183cm
-        """
-
-        # then
-        assert is_passport_valid(passport) is False
-
     def test_when_passport_is_invalid_when_eyr_is_missing(self):
         # given
         passport = """
@@ -42,7 +23,6 @@ byr:1937 iyr:2017 cid:147 hgt:183cm
 
         # then
         assert is_passport_valid(passport) is False
-
 
     def test_when_passport_is_invalid_when_hgt_is_missing(self):
         # given
@@ -58,17 +38,6 @@ byr:1937 iyr:2017 cid:147
         # given
         passport = """
 ecl:gry pid:860033327 eyr:2020
-byr:1937 iyr:2017 cid:147 hgt:183cm
-        """
-
-        # then
-        assert is_passport_valid(passport) is False
-
-
-    def test_when_passport_is_invalid_when_ecl_is_missing(self):
-        # given
-        passport = """
-pid:860033327 eyr:2020 hcl:#fffffd
 byr:1937 iyr:2017 cid:147 hgt:183cm
         """
 
@@ -95,7 +64,6 @@ byr:1937 iyr:2017 hgt:183cm
         # then
         assert is_passport_valid(passport)
 
-
     def test_is_valid_when_passport_is_from_north_pole_but_containing_every_required_field(self):
         # given
         passport = """
@@ -107,3 +75,294 @@ hgt:179cm
 
         # then
         assert is_passport_valid(passport) is True
+
+    @pytest.mark.parametrize("passport", [
+        ("""
+        eyr:1972 cid:100
+        hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926
+        """),
+        ("""
+        iyr:2019
+        hcl:#602927 eyr:1967 hgt:170cm
+        ecl:grn pid:012533040 byr:1946
+        """),
+        ("""
+        hcl:dab227 iyr:2012
+        ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277
+        """),
+    ])
+    def test_passport_is_invalid_with_given_example(self, passport):
+        assert is_passport_valid(passport) is False
+
+    @pytest.mark.parametrize("passport", [
+        ("""
+eyr:2029 ecl:blu cid:129 byr:1989
+iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm
+        """),
+        ("""
+hcl:#888785
+hgt:164cm byr:2001 iyr:2015 cid:88
+pid:545766238 ecl:hzl
+eyr:2022
+        """),
+        ("""
+iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719
+        """)
+    ])
+    def test_passport_is_valid_with_given_example(self, passport):
+        assert is_passport_valid(passport) is True
+
+
+class TestByr:
+    def test_when_passport_is_invalid_when_byr_is_missing(self):
+        # given
+        passport = """
+    ecl:gry pid:860033327 eyr:2020 hcl:#fffffd
+    iyr:2017 cid:147 hgt:183cm
+            """
+
+        # then
+        assert is_passport_valid(passport) is False
+
+    def test_when_passport_a_valid_birthdate(self):
+        # given
+        passport = """
+    ecl:gry pid:860033327 eyr:2020 hcl:#fffffd
+    iyr:2017 cid:147 hgt:183cm byr:2002
+            """
+
+        # then
+        assert is_passport_valid(passport) is True
+
+    def test_when_people_is_too_young(self):
+        # given
+        passport = """
+    ecl:gry pid:860033327 eyr:2020 hcl:#fffffd
+    iyr:2017 cid:147 hgt:183cm byr:2003
+            """
+
+        # then
+        assert is_passport_valid(passport) is False
+
+    def test_when_people_is_too_old(self):
+        # given
+        passport = """
+    ecl:gry pid:860033327 eyr:2020 hcl:#fffffd
+    iyr:2017 cid:147 hgt:183cm byr:1919
+            """
+
+        # then
+        assert is_passport_valid(passport) is False
+
+
+class TestIyr:
+    def test_when_passport_is_invalid_when_iyr_is_missing(self):
+        # given
+        passport = """
+    ecl:gry pid:860033327 eyr:2020 hcl:#fffffd
+    byr:1937 cid:147 hgt:183cm
+            """
+
+        # then
+        assert is_passport_valid(passport) is False
+
+    def test_when_passport_a_valid_birthdate(self):
+        # given
+        passport = """
+    ecl:gry pid:860033327 eyr:2020 hcl:#fffffd
+    iyr:2010 cid:147 hgt:183cm byr:2001
+            """
+
+        # then
+        assert is_passport_valid(passport) is True
+
+    def test_when_passport_is_too_old(self):
+        # given
+        passport = """
+    ecl:gry pid:860033327 eyr:2020 hcl:#fffffd
+    iyr:2009 cid:147 hgt:183cm byr:2001
+            """
+
+        # then
+        assert is_passport_valid(passport) is False
+
+    def test_when_passport_is_too_young(self):
+        # given
+        passport = """
+    ecl:gry pid:860033327 eyr:2020 hcl:#fffffd
+    iyr:2021 cid:147 hgt:183cm byr:2001
+            """
+
+        # then
+        assert is_passport_valid(passport) is False
+
+
+class TestEyr:
+    def test_when_passport_is_invalid_when_eyr_is_missing(self):
+        # given
+        passport = """
+ecl:gry pid:860033327 hcl:#fffffd
+byr:1937 iyr:2017 cid:147 hgt:183cm
+        """
+
+        # then
+        assert is_passport_valid(passport) is False
+
+    def test_when_passport_is_invalid_when_eyr_is_valid(self):
+        # given
+        passport = """
+ecl:gry pid:860033327 hcl:#fffffd eyr:2021
+byr:1937 iyr:2017 cid:147 hgt:183cm
+        """
+
+        # then
+        assert is_passport_valid(passport) is True
+
+    def test_when_passport_is_expired(self):
+        # given
+        passport = """
+ecl:gry pid:860033327 hcl:#fffffd eyr:2019
+byr:1937 iyr:2017 cid:147 hgt:183cm
+        """
+
+        # then
+        assert is_passport_valid(passport) is False
+
+    def test_when_passport_is_expiring_to_late(self):
+        # given
+        passport = """
+ecl:gry pid:860033327 hcl:#fffffd eyr:2031
+byr:1937 iyr:2017 cid:147 hgt:183cm
+        """
+
+        # then
+        assert is_passport_valid(passport) is False
+
+
+class TestHgt:
+    def test_74in_is_valid_height(self):
+        # given
+        passport = """
+        pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980
+        hcl:#623a2f
+        """
+
+        # then
+        assert is_passport_valid(passport)
+
+    def test_77in_is_invalid_height(self):
+        # given
+        passport = """
+        pid:087499704 hgt:77in ecl:grn iyr:2012 eyr:2030 byr:1980
+        hcl:#623a2f
+        """
+
+        # then
+        assert is_passport_valid(passport) is False
+
+    def test_58in_is_invalid_height(self):
+        # given
+        passport = """
+        pid:087499704 hgt:58in ecl:grn iyr:2012 eyr:2030 byr:1980
+        hcl:#623a2f
+        """
+
+        # then
+        assert is_passport_valid(passport) is False
+
+    def test_190cm_is_valid_height(self):
+        # given
+        passport = """
+        pid:087499704 hgt:190cm ecl:grn iyr:2012 eyr:2030 byr:1980
+        hcl:#623a2f
+        """
+
+        # then
+        assert is_passport_valid(passport)
+
+    def test_149cm_is_invalid_height(self):
+        # given
+        passport = """
+        pid:087499704 hgt:149cm ecl:grn iyr:2012 eyr:2030 byr:1980
+        hcl:#623a2f
+        """
+
+        # then
+        assert is_passport_valid(passport) is False
+
+    def test_194cm_is_invalid_height(self):
+        # given
+        passport = """
+        pid:087499704 hgt:194cm ecl:grn iyr:2012 eyr:2030 byr:1980
+        hcl:#623a2f
+        """
+
+        # then
+        assert is_passport_valid(passport) is False
+
+    def test_is_invalid_height_when_no_unit(self):
+        # given
+        passport = """
+        pid:087499704 hgt:190 ecl:grn iyr:2012 eyr:2030 byr:1980
+        hcl:#623a2f
+        """
+
+        # then
+        assert is_passport_valid(passport) is False
+
+
+class TestHcl:
+    @pytest.mark.parametrize("color,is_valid", [
+        ("#123abc", True),
+        ("#fffffd", True),
+        ("#123abz", False),
+        ("123abc", False),
+    ])
+    def test_check_hair_color(self, color, is_valid):
+        # given
+        passport = f"""
+        pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980
+        hcl:{color}
+        """
+
+        # then
+        assert is_passport_valid(passport) == is_valid
+
+
+class TestEcl:
+    @pytest.mark.parametrize("color,is_valid", [
+        ('amb', True),
+        ('blu', True),
+        ('brn', True),
+        ('gry', True),
+        ('grn', True),
+        ('hzl', True),
+        ('oth', True),
+        ("wat", False),
+    ])
+    def test_check_hair_color(self, color, is_valid):
+        # given
+        passport = f"""
+        pid:087499704 hgt:74in ecl:{color} iyr:2012 eyr:2030 byr:1980
+        hcl:#123abc
+        """
+
+        # then
+        assert is_passport_valid(passport) == is_valid
+
+
+class TestPid:
+    @pytest.mark.parametrize("pid,is_valid", [
+        ('000000001', True),
+        ('087499704', True),
+        ('0123456789', False),
+    ])
+    def test_check_pid(self, pid, is_valid):
+        # given
+        passport = f"""
+        pid:{pid} hgt:74in ecl:hzl iyr:2012 eyr:2030 byr:1980
+        hcl:#123abc
+        """
+
+        # then
+        assert is_passport_valid(passport) == is_valid
