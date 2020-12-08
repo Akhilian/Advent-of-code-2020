7e1cfac5a0353313f26dc13c4691bc17799c6f00
Adrien SAUNIER <contact.adriensaunier@gmail.com>
2020-12-08 18:32:48 +0100

    :sparkles: Day 5 - Ex 1

---
+++ day_5_boarding_pass/boarding_pass.py
@@ -0,0 +1,54 @@
+from common.file_reader import FileReader
+
+
+class BoardingPass():
+    def __init__(self, sequence: str):
+        self._sequence = sequence
+
+    def get_row(self) -> int:
+        rows = list(range(0, 128))
+        positions = iter(self._sequence.strip()[0:7])
+
+        while True:
+            try:
+                position = next(positions)
+                is_upper_half = position.startswith('F')
+                limit = int(len(rows) / 2)
+
+                if is_upper_half:
+                    rows = rows[0:limit]
+                else:
+                    rows = rows[limit:]
+            except StopIteration:
+                break
+
+        return rows[0]
+
+    def get_column(self) -> int:
+        rows = list(range(0, 8))
+        positions = iter(self._sequence.strip()[7:])
+
+        while True:
+            try:
+                position = next(positions)
+                is_upper_half = position.startswith('L')
+                limit = int(len(rows) / 2)
+
+                if is_upper_half:
+                    rows = rows[0:limit]
+                else:
+                    rows = rows[limit:]
+            except StopIteration:
+                break
+
+        return max(rows)
+
+    def get_seat_id(self) -> int:
+        return self.get_row() * 8 + self.get_column()
+
+
+if __name__ == '__main__':
+    file_reader = FileReader('./input')
+    sequences = file_reader.to_str_list()
+    list_of_seats_id = [BoardingPass(sequence).get_seat_id() for sequence in sequences]
+    print(max(list_of_seats_id))
---
+++ day_5_boarding_pass/boarding_pass_test.py
@@ -0,0 +1,105 @@
+from day_5_boarding_pass.boarding_pass import BoardingPass
+
+
+class TestColumn:
+    def test_should_be_column_5_with_full_sequence(self):
+        # given
+        boarding_pass = BoardingPass('FBFBBFFRLR')
+
+        # then
+        assert boarding_pass.get_column() == 5
+
+    def test_should_be_column_7_with_full_sequence(self):
+        # given
+        boarding_pass = BoardingPass('BFFFBBFRRR')
+
+        # then
+        assert boarding_pass.get_column() == 7
+
+    def test_should_be_column_0_with_full_sequence(self):
+        # given
+        boarding_pass = BoardingPass('BFFFBBFLLL')
+
+        # then
+        assert boarding_pass.get_column() == 0
+
+
+class TestGetRows:
+    def test_when_starting_with_F_should_be_in_the_front(self):
+        # given
+        boarding_pass = BoardingPass('F')
+
+        # then
+        assert boarding_pass.get_row() <= 63
+
+    def test_when_starting_with_B_should_be_in_the_back(self):
+        # given
+        boarding_pass = BoardingPass('B')
+
+        # then
+        assert boarding_pass.get_row() > 63
+
+    def test_when_in_front_B_means_upper_half(self):
+        # given
+        boarding_pass = BoardingPass('FB')
+
+        # then
+        row = boarding_pass.get_row()
+        assert row <= 63
+        assert row >= 32
+
+    def test_when_in_lowerfront_F_means_lower_half(self):
+        # given
+        boarding_pass = BoardingPass('FBF')
+
+        # then
+        row = boarding_pass.get_row()
+        assert row <= 47
+        assert row >= 32
+
+    def test_when_in_another_B_means_lower_half(self):
+        # given
+        boarding_pass = BoardingPass('FBFB')
+
+        # then
+        row = boarding_pass.get_row()
+        assert row <= 47
+        assert row >= 40
+
+
+class TestBoardingPass:
+    def test_should_be_raw_44_with_full_sequence(self):
+        # given
+        boarding_pass = BoardingPass('FBFBBFFRLR')
+
+        # then
+        assert boarding_pass.get_row() == 44
+        assert boarding_pass.get_column() == 5
+        assert boarding_pass.get_seat_id() == 357
+
+    def test_should_be_raw_70_with_full_sequence(self):
+        # given
+        boarding_pass = BoardingPass('BFFFBBFRRR')
+
+        # then
+        assert boarding_pass.get_row() == 70
+        assert boarding_pass.get_column() == 7
+        assert boarding_pass.get_seat_id() == 567
+
+    def test_should_be_raw_14_with_full_sequence(self):
+        # given
+        boarding_pass = BoardingPass('FFFBBBFRRR')
+
+        # then
+        assert boarding_pass.get_row() == 14
+        assert boarding_pass.get_column() == 7
+        assert boarding_pass.get_seat_id() == 119
+
+    def test_should_be_raw_102_with_full_sequence(self):
+        # given
+        boarding_pass = BoardingPass('BBFFBBFRLL')
+
+        # then
+        assert boarding_pass.get_row() == 102
+        assert boarding_pass.get_column() == 4
+        assert boarding_pass.get_seat_id() == 820
